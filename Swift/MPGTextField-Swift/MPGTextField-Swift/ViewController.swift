41c51a5b96a590b175b0251292587534c92c053b
Jason Hurt <jleehurt@gmail.com>
2015-07-10 10:37:19 -0700

    update to Swift 1.2. Only alloc tableview once. Only add gesture recognizer once.

--- Swift/MPGTextField-Swift/MPGTextField-Swift/ViewController.swift
+++ Swift/MPGTextField-Swift/MPGTextField-Swift/ViewController.swift
@@ -10,14 +10,14 @@ import UIKit
 
 class ViewController: UIViewController, MPGTextFieldDelegate {
     
-    var sampleData = Dictionary<String, AnyObject>[]()
-    @IBOutlet var name : MPGTextField_Swift
+    var sampleData = [Dictionary<String, AnyObject>]()
+    @IBOutlet var name : MPGTextField_Swift!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.generateData()
-        name.mDelegate = self
+        name.mpgTextFieldDelegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,34 +25,31 @@ class ViewController: UIViewController, MPGTextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    func generateData(){
-        var err : NSErrorPointer?
+    func generateData() {
+        var err : NSErrorPointer = NSErrorPointer()
         var dataPath = NSBundle.mainBundle().pathForResource("sample_data", ofType: "json")
-        var data = NSData.dataWithContentsOfFile(dataPath, options: NSDataReadingOptions.DataReadingUncached, error: err!)
-        var contents : AnyObject[]! = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: err!) as AnyObject[]
-        //println(contents[0]["first_name"])
-        for var i = 0;i<contents.count;++i{
-            var name = contents[i]["first_name"] as String
-            var lName = contents[i]["last_name"] as String
+        var data = NSData(contentsOfFile: dataPath!, options: NSDataReadingOptions.DataReadingUncached, error: err)
+        var contents :[AnyObject]! = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments, error: err) as! [AnyObject]
+        for datum in contents {
+            var name = datum["first_name"] as! String
+            var lName = datum["last_name"] as! String
             name += " " + lName
-            var email = contents[i]["email"] as String
-            var dictionary = ["DisplayText":name,"DisplaySubText":email,"CustomObject":contents[i]]
-        
+            var email = datum["email"] as! String
+            var dictionary = ["DisplayText":name,"DisplaySubText":email,"CustomObject":datum]
             sampleData.append(dictionary)
-            }
-
+        }
     }
 
-    func dataForPopoverInTextField(textfield: MPGTextField_Swift) -> Dictionary<String, AnyObject>[]
-    {
+    // MARK: MPGTextFieldDelegate
+    func dataForPopoverInTextField(textfield: MPGTextField_Swift) -> [Dictionary<String, AnyObject>] {
         return sampleData
     }
     
-    func textFieldShouldSelect(textField: MPGTextField_Swift) -> Bool{
+    func textFieldShouldSelect(textField: MPGTextField_Swift) -> Bool {
         return true
     }
 
-    func textFieldDidEndEditing(textField: MPGTextField_Swift, withSelection data: Dictionary<String,AnyObject>){
+    func textFieldDidEndEditing(textField: MPGTextField_Swift, withSelection data: Dictionary<String,AnyObject>) {
         println("Dictionary received = \(data)")
     }
 }
--- Swift/MPGTextField/MPGTextField-Swift.swift
+++ Swift/MPGTextField/MPGTextField-Swift.swift
@@ -8,18 +8,18 @@
 
 import UIKit
 
-@objc protocol MPGTextFieldDelegate{
-    func dataForPopoverInTextField(textfield: MPGTextField_Swift) -> Dictionary<String, AnyObject>[]
-
-    @optional func textFieldDidEndEditing(textField: MPGTextField_Swift, withSelection data: Dictionary<String,AnyObject>)
-    @optional func textFieldShouldSelect(textField: MPGTextField_Swift) -> Bool
+@objc protocol MPGTextFieldDelegate {
+    func dataForPopoverInTextField(textfield: MPGTextField_Swift) -> [Dictionary<String, AnyObject>]
+    
+    optional func textFieldDidEndEditing(textField: MPGTextField_Swift, withSelection data: Dictionary<String,AnyObject>)
+    optional func textFieldShouldSelect(textField: MPGTextField_Swift) -> Bool
 }
 
 class MPGTextField_Swift: UITextField, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource, UIGestureRecognizerDelegate {
     
-    var mDelegate : MPGTextFieldDelegate?
+    var mpgTextFieldDelegate : MPGTextFieldDelegate?
     var tableViewController : UITableViewController?
-    var data = Dictionary<String, AnyObject>[]()
+    var data = [Dictionary<String, AnyObject>]()
     
     //Set this to override the default color of suggestions popover. The default color is [UIColor colorWithWhite:0.8 alpha:0.9]
     @IBInspectable var popoverBackgroundColor : UIColor = UIColor(red: 240.0/255.0, green: 240.0/255.0, blue: 240.0/255.0, alpha: 1.0)
@@ -29,133 +29,119 @@ class MPGTextField_Swift: UITextField, UITextFieldDelegate, UITableViewDelegate,
     
     //Set this to override the default seperator color for tableView in search results. The default color is light gray.
     @IBInspectable var seperatorColor : UIColor = UIColor(white: 0.95, alpha: 1.0)
-
-
-    init(frame: CGRect) {
-        super.init(frame: frame)
-        // Initialization code
-    }
     
-    init(coder aDecoder: NSCoder!){
-        super.init(coder: aDecoder)
-    }
-
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect)
-    {
-        // Drawing code
-    }
-    */
-    
-    override func layoutSubviews(){
+    override func layoutSubviews() {
         super.layoutSubviews()
         let str : String = self.text
         
-        if (countElements(str) > 0) && (self.isFirstResponder())
-        {
-            if mDelegate{
-                data = mDelegate!.dataForPopoverInTextField(self)
+        if count(str) > 0 && self.isFirstResponder() {
+            if mpgTextFieldDelegate != nil {
+                data = mpgTextFieldDelegate!.dataForPopoverInTextField(self)
                 self.provideSuggestions()
             }
-            else{
-                println("<MPGTextField> WARNING: You have not implemented the requred methods of the MPGTextField protocol.")
-            }
-        }
-        else{
-            if let table = self.tableViewController{
-                if table.tableView.superview != nil{
-                    table.tableView.removeFromSuperview()
-                    self.tableViewController = nil
-                }
+            else {
+                println("mpgTextFieldDelegate is nil")
             }
         }
     }
     
-    override func resignFirstResponder() -> Bool{
+    func createTableView() {
+        tableViewController = UITableViewController.alloc()
+        tableViewController!.tableView.delegate = self
+        tableViewController!.tableView.dataSource = self
+        tableViewController!.tableView.backgroundColor = popoverBackgroundColor
+        tableViewController!.tableView.separatorColor = seperatorColor
+        if let frameSize = popoverSize {
+            self.tableViewController!.tableView.frame = frameSize
+        }
+        else {
+            //PopoverSize frame has not been set. Use default parameters instead.
+            var frameForPresentation = frame
+            frameForPresentation.origin.y += frame.size.height
+            frameForPresentation.size.height = 200
+            tableViewController!.tableView.frame = frameForPresentation
+        }
+        
+        var frameForPresentation = frame
+        frameForPresentation.origin.y += frame.size.height;
+        frameForPresentation.size.height = 200;
+        tableViewController!.tableView.frame = frameForPresentation
+        superview!.addSubview(tableViewController!.tableView)
+    }
+    
+    func createGestureRecognizer() {
+        //Add a tap gesture recogniser to dismiss the suggestions view when the user taps outside the suggestions view
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: "didTouchSuperview:")
+        tapRecognizer.numberOfTapsRequired = 1
+        tapRecognizer.cancelsTouchesInView = false
+        tapRecognizer.delegate = self
+        self.superview!.addGestureRecognizer(tapRecognizer)
+    }
+    
+    func showSuggestions() {
+        if tableViewController == nil {
+            createTableView()
+            createGestureRecognizer()
+        }
+        tableViewController!.tableView.reloadData()
         UIView.animateWithDuration(0.3,
             animations: ({
+                self.tableViewController!.tableView.alpha = 1.0
+            }),
+            completion :{
+                (finished : Bool) in
+        })
+    }
+    
+    func hideSuggestions() {
+        if tableViewController != nil {
+            UIView.animateWithDuration(0.3,
+                animations: ({
                     self.tableViewController!.tableView.alpha = 0.0
                 }),
-            completion:{
+                completion:{
                     (finished : Bool) in
-                    self.tableViewController!.tableView.removeFromSuperview()
-                    self.tableViewController = nil
-                })
-        self.handleExit()
+            })
+        }
+    }
+    
+    override func resignFirstResponder() -> Bool {
+        hideSuggestions()
         return super.resignFirstResponder()
     }
     
-    func provideSuggestions(){
-        if let tvc = self.tableViewController {
-            tableViewController!.tableView.reloadData()
+    func provideSuggestions() {
+        if self.applyFilterWithSearchQuery(self.text).count > 0 {
+            showSuggestions()
         }
-        else if self.applyFilterWithSearchQuery(self.text).count > 0{
-            //Add a tap gesture recogniser to dismiss the suggestions view when the user taps outside the suggestions view
-            let tapRecognizer = UITapGestureRecognizer(target: self, action: "tapped:")
-            tapRecognizer.numberOfTapsRequired = 1
-            tapRecognizer.cancelsTouchesInView = false
-            tapRecognizer.delegate = self
-            self.superview.addGestureRecognizer(tapRecognizer)
-
-            self.tableViewController = UITableViewController.alloc()
-            self.tableViewController!.tableView.delegate = self
-            self.tableViewController!.tableView.dataSource = self
-            self.tableViewController!.tableView.backgroundColor = self.popoverBackgroundColor
-            self.tableViewController!.tableView.separatorColor = self.seperatorColor
-            if let frameSize = self.popoverSize{
-                self.tableViewController!.tableView.frame = frameSize
-            }
-            else{
-                //PopoverSize frame has not been set. Use default parameters instead.
-                var frameForPresentation = self.frame
-                frameForPresentation.origin.y += self.frame.size.height
-                frameForPresentation.size.height = 200
-                self.tableViewController!.tableView.frame = frameForPresentation
-            }
-            
-            var frameForPresentation = self.frame
-            frameForPresentation.origin.y += self.frame.size.height;
-            frameForPresentation.size.height = 200;
-            tableViewController!.tableView.frame = frameForPresentation
-            
-            self.superview.addSubview(tableViewController!.tableView)
-            self.tableViewController!.tableView.alpha = 0.0
-            UIView.animateWithDuration(0.3,
-                animations: ({
-                    self.tableViewController!.tableView.alpha = 1.0
-                    }),
-                completion:{
-                    (finished : Bool) in
-                    
-                })
+        else {
+            hideSuggestions()
         }
-        
     }
     
-    func tapped (sender : UIGestureRecognizer!){
-        if let table = self.tableViewController{
-            if !CGRectContainsPoint(table.tableView.frame, sender.locationInView(self.superview)) && self.isFirstResponder(){
+    func didTouchSuperview(sender : UIGestureRecognizer!) {
+        if let table = self.tableViewController {
+            if !CGRectContainsPoint(table.tableView.frame, sender.locationInView(self.superview)) && self.isFirstResponder() {
                 self.resignFirstResponder()
             }
         }
     }
     
-    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int{
+    // MARK: UITableViewDataSource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         var count = self.applyFilterWithSearchQuery(self.text).count
-        if count == 0{
+        if count == 0 {
             UIView.animateWithDuration(0.3,
                 animations: ({
                     self.tableViewController!.tableView.alpha = 0.0
-                    }),
+                }),
                 completion:{
                     (finished : Bool) in
-                    if let table = self.tableViewController{
+                    if let table = self.tableViewController {
                         table.tableView.removeFromSuperview()
                         self.tableViewController = nil
                     }
-                })
+            })
         }
         return count
     }
@@ -163,60 +149,40 @@ class MPGTextField_Swift: UITextField, UITextFieldDelegate, UITableViewDelegate,
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCellWithIdentifier("MPGResultsCell") as? UITableViewCell
         
-        if !cell{
+        if cell == nil {
             cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MPGResultsCell")
         }
-
+        
         cell!.backgroundColor = UIColor.clearColor()
         let dataForRowAtIndexPath = self.applyFilterWithSearchQuery(self.text)[indexPath.row]
         let displayText : AnyObject? = dataForRowAtIndexPath["DisplayText"]
         let displaySubText : AnyObject? = dataForRowAtIndexPath["DisplaySubText"]
-        cell!.textLabel.text = displayText as String
-        cell!.detailTextLabel.text = displaySubText as String
+        cell!.textLabel!.text = displayText as? String
+        cell!.detailTextLabel!.text = displaySubText as? String
         
         return cell!
     }
     
-    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!){
-        //self.text = self.applyFilterWithSearchQuery(self.text)[indexPath.row]["DisplayText"]
+    
+    // MARK: UITableViewDelegate
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let selectedData = self.applyFilterWithSearchQuery(self.text)[indexPath.row]
+        self.text = selectedData["DisplayText"] as! String
+        mpgTextFieldDelegate?.textFieldDidEndEditing?(self, withSelection: selectedData)
         self.resignFirstResponder()
     }
     
-    
-//   #pragma mark Filter Method
-    
-    func applyFilterWithSearchQuery(filter : String) -> Dictionary<String, AnyObject>[]
-    {
-        //let predicate = NSPredicate(format: "DisplayText BEGINSWITH[cd] \(filter)")
+    // MARK: Filter Method
+    func applyFilterWithSearchQuery(filter : String) -> [Dictionary<String, AnyObject>] {
         var lower = (filter as NSString).lowercaseString
         var filteredData = data.filter({
-                if let match : AnyObject  = $0["DisplayText"]{
-                    //println("LCS = \(filter.lowercaseString)")
-                    return (match as NSString).lowercaseString.hasPrefix((filter as NSString).lowercaseString)
-                }
-                else{
-                    return false
-                }
-            })
+            if let match : AnyObject  = $0["DisplayText"] {
+                return (match as! NSString).lowercaseString.hasPrefix((filter as NSString).lowercaseString)
+            }
+            else {
+                return false
+            }
+        })
         return filteredData
     }
-    
-    func handleExit(){
-        if let table = self.tableViewController{
-            table.tableView.removeFromSuperview()
-        }
-        if mDelegate?.textFieldShouldSelect?(self){
-            if self.applyFilterWithSearchQuery(self.text).count > 0 {
-                let selectedData = self.applyFilterWithSearchQuery(self.text)[0]
-                let displayText : AnyObject? = selectedData["DisplayText"]
-                self.text = displayText as String
-                mDelegate?.textFieldDidEndEditing?(self, withSelection: selectedData)
-            }
-            else{
-                mDelegate?.textFieldDidEndEditing?(self, withSelection: ["DisplayText":self.text, "CustomObject":"NEW"])
-            }
-        }
-
-    }
-
 }
